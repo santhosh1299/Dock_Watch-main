b53f0ebd62cd81789c1391d9bb5cbea3363bbe0c
santhosh1299 <santhoshmanoharan1299@gmail.com>
2024-03-23 21:05:58 -0600

    upload to git

---
+++ 02. Code/01_Data_collecion_from_GBFS.py
@@ -0,0 +1,71 @@
+import urllib.request
+import json
+import pandas as pd
+import datetime, pytz
+import schedule
+import time
+
+# Urls
+url_gbfs= "https://gbfs.bcycle.com/bcycle_boulder/gbfs.json"
+url_gbfs_versions= "https://gbfs.bcycle.com/bcycle_boulder/gbfs_versions.json"
+url_system_information= "https://gbfs.bcycle.com/bcycle_boulder/system_information.json"
+url_station_information= "https://gbfs.bcycle.com/bcycle_boulder/station_information.json"
+url_station_status= "https://gbfs.bcycle.com/bcycle_boulder/station_status.json"
+url_system_pricing_plans= "https://gbfs.bcycle.com/bcycle_boulder/system_pricing_plans.json"
+url_system_regions= "https://gbfs.bcycle.com/bcycle_boulder/system_regions.json"
+
+
+def data_collection():
+    # Reading station information document and extracting the required data
+    with urllib.request.urlopen(url_station_information) as url:
+        data_station = json.load(url)
+        #print(data_station)
+
+    df_stations=pd.DataFrame(i for i in data_station['data']['stations'])
+    df_stations['last_updated']= data_station['last_updated']
+    df_stations.drop(columns= ['rental_uris'],inplace=True)
+    #df_stations.head()
+
+    # Reading station status information document and extracting the required data
+    with urllib.request.urlopen(url_station_status) as url:
+        data_status = json.load(url)
+        #print(data_status)
+
+    df_status=pd.DataFrame(i for i in data_status['data']['stations'])
+    df_status['electic_bikes_available']= [i['num_bikes_available_types']['electric'] for i in data_status['data']['stations']]
+    df_status['smart_bikes_available']= [i['num_bikes_available_types']['smart'] for i in data_status['data']['stations']]
+    df_status['classic_bikes_available']= [i['num_bikes_available_types']['classic'] for i in data_status['data']['stations']]
+    df_status.drop(columns='num_bikes_available_types',inplace= True)
+    #df_status.head()
+
+    # Merging the stations and status data 
+    df_merged= pd.merge(df_stations,df_status,on='station_id',how='outer')
+
+    # Reordering and renaming the columns
+    column_order = ["last_updated", "last_reported", "station_id", "name", "address", "lon", "lat", "is_returning", "is_renting", "is_installed", "_bcycle_station_type", "num_docks_available", "num_bikes_available", "electic_bikes_available", "smart_bikes_available", "classic_bikes_available"]
+    column_rename= {"last_updated": "station_last_updated", "last_reported": "status_last_reported", "name":"station_name","address":"station_address","lon":"station_longitude","lat": "station_latitude","is_returning":"station_is_returning","is_renting":"station_is_renting","is_installed":"station_is_installed","_bcycle_station_type":"station_type","num_docks_available":"docks_available","num_bikes_available":"bikes_available"}
+    df_merged= df_merged[column_order]
+    df_merged.rename(columns = column_rename, inplace = True)
+    time_of_collection= datetime.datetime.now(pytz.timezone('US/Mountain')).strftime('%m-%d-%y %H:%M:%S')
+    df_merged['datatime_mtd']= time_of_collection
+    #df_merged.head()
+
+    # Concatenate the new data to the existing data
+    df_existing= pd.read_csv(r"C:\Data Mining BCycle Data Collection\Bcycle_data_to_date.csv")
+    df_final= pd.concat([df_existing,df_merged])
+
+    df_final.to_csv(r"C:\Data Mining BCycle Data Collection\Bcycle_data_to_date.csv", index=False)
+
+    print("Completed Data Time: ",time_of_collection)
+
+    # Recording logs 
+    log= pd.read_csv(r"C:\Data Mining BCycle Data Collection\Automation_logs.csv")
+    new_log = pd.Series([df_merged['datatime_mtd'][0], df_merged.shape[0],df_final.shape[0]], index=log.columns)
+    log = log._append(new_log,ignore_index=True)
+    log.to_csv(r"C:\Data Mining BCycle Data Collection\Automation_logs.csv", index=False)
+
+schedule.every(3).minutes.do(data_collection)
+
+while 1:
+    schedule.run_pending()
+    time.sleep(3)
---
+++ README.md
@@ -0,0 +1,29 @@
+# Dock Watch
+
+**About:**
+Boulder BCycle is a popular and convenient transportation service in Boulder, Colorado. It offers electric bikes stationed throughout the city, providing a sustainable and efficient means of commuting. Notably, the service is tailored to the needs of the community, as it offers a unique benefit to students by providing them with free rides for up to one hour per trip. This student-friendly policy makes it an attractive choice for the University of Colorado Boulder's student population, who frequently utilize Boulder BCycle to commute between their residences and the university. The service plays a key role in the city's transport system, supporting sustainability goals and meeting student needs.
+
+In Boulder's BCycle system, users often encounter reliability issues and struggle to predict bike station availability. This project, as part of the **CSCI 5502- Data Mining** course, **focuses on transforming the BCycle experience by dissecting usage patterns and calculating waiting times for each station**. By analyzing historical data and real-time inputs, we aim to provide users with valuable insights, enabling them to make informed decisions about when and where to pick up a bike. This initiative seeks to enhance the overall convenience and efficiency of the service, ultimately making BCycle an even more reliable and accessible mode of transportation for the Boulder community.
+
+[Presentation Link](https://www.youtube.com/watch?v=QEBMTk4OsAE)       [Dashboard Link](https://public.tableau.com/views/DockWatch/DockWatch?:language=en-GB&publish=yes&:display_count=n&:origin=viz_share_link)
+
+**Challenges:**
+* **Data Availability**: Historical BCycle station status data is currently unavailable, as only real-time data is accessible. Consequently, we've been consistently collecting data in the background for our analysis.
+* **Data Inconsistency**: The data is gathered continuously in the background using an AWS EC2 instance, but it occasionally experiences interruptions, causing breaks in the data. External factors such as Colorado's Daylight Saving Time also introduce additional complexity.
+* **Data Sampling Limitations**: Numerous variables, such as public transport availability, weather conditions, and class schedules, influence BCycle usage. It's important to acknowledge that our data sample may not encompass all these influencing factors
+
+**Approach Overview:**
+1. **Data Collection & Storage**: Collect live data from GBFS (General Bikeshare Feed Specification) and store it in an SQL server to maintain historical records.
+2. **Data Preprocessing Pipeline**: Develop a data preprocessing pipeline to calculate additional features and ensure data quality.
+3. **Exploratory Data Analysis**: Conduct Exploratory Data Analysis (EDA) using a dashboard to identify and understand usage patterns within the dataset.
+4. **Forecasting Model Development**: Build a predictive model to forecast waiting times for each station, utilizing the pre-processed data.
+
+**Data Flow Architecture**
+
+![image](https://github.com/Niranjan-Cholendiran/Dock_Watch/assets/78549555/918cd3c9-5492-49f0-9b64-d1bce56b77f7)
+
+
+**Future Work:**
+1. **Dashboard Creation**: Develop a dashboard to visualize real-time and historical bike station data along with live wait times.
+2. **Cloud Deployment**: Deploy the Tableau dashboard, forecasting model, data preprocessing code, and data collection code to a cloud platform.
+3. **Automation**: Automate the entire data collection, preprocessing, modeling, and dashboard updating processes to execute them based on a trigger point within the dashboard.
